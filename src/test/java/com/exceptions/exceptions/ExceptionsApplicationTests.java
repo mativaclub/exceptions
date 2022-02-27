754099ca953946b1469190cdfd089a8068966943
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-02-27 21:54:05 +0200

    Initial commit

---
+++ src/test/java/com/exceptions/exceptions/ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exceptions.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
